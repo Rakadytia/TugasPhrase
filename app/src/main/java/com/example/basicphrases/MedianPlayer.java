8fd20b5ba4e5e3e7aa162908178071bb4b0b835f
rakadytia <rakaakbar.58@gmail.com>
2022-04-27 21:51:46 +0700

    main

---
+++ app/src/main/java/com/example/basicphrases/MedianPlayer.java
@@ -0,0 +1,12 @@
+package com.example.basicphrases;
+
+public class MedianPlayer {
+    public static MedianPlayer create( MainActivity mainActivity, int resorceId ) {
+        MedianPlayer medianPlayer = null;
+        MedianPlayer medianPlayer1 = medianPlayer;
+        return medianPlayer1;
+    }
+
+        public static void start() {
+    }
+}
